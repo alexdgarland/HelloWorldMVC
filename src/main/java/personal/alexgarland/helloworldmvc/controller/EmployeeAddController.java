78277441c81f17e6415ae66c252ebf816fa0e1e8
Alex Garland <alex.d.garland@gmail.com>
2018-04-15 01:08:07 +0100

    Autowire employeeManager in EmployeeAddController

--- src/main/java/personal/alexgarland/helloworldmvc/controller/EmployeeAddController.java
+++ src/main/java/personal/alexgarland/helloworldmvc/controller/EmployeeAddController.java
@@ -1,15 +1,19 @@
 package personal.alexgarland.helloworldmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import personal.alexgarland.helloworldmvc.model.Employee;
-import personal.alexgarland.helloworldmvc.service.FixedListEmployeeManager; 
+import personal.alexgarland.helloworldmvc.service.IEmployeeManager; 
 
 @Controller
 public class EmployeeAddController {
  
+	@Autowired
+	private IEmployeeManager employeeManager;
+	
 	@RequestMapping("/showEmployeeForm")
 	public ModelAndView getEmployeeForm() {
 		ModelAndView mv = new ModelAndView("employeeAdd");
@@ -19,7 +23,7 @@ public class EmployeeAddController {
  
 	@RequestMapping("/addEmployee")
 	public ModelAndView addEmployee(@ModelAttribute Employee e) throws CloneNotSupportedException {
-		new FixedListEmployeeManager().addEmployee(e);
+		employeeManager.addEmployee(e);
 		return new ModelAndView("redirect:employees");
 	}
 }
